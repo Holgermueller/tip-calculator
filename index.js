81178717906b195f9b386c7d4162554bf16492d8
holgermueller <9jholger@gmail.com>
2024-06-10 13:42:02 -0400

    got reset button working

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ let numberOfPeopleEntered = document.getElementById("numberOfPeople");
 
 const divideAmongstGroup = () => {
   //Get bill
-  const billValue = billInput.value;
+  let billValue = billInput.value;
   console.log(billValue);
 
   //add tip
   //document.querySelector('input[name="genderS"]:checked').value;
-  if (document.getElementById("input[name='name']:checked").value == true) {
-    console.log("checked 5");
+
+  if (document.getElementById("five").checked) {
+    billValue = parseInt(billValue) + parseInt(billValue) * 0.05;
+    console.log(billValue);
+  }
+  if (document.getElementById("ten").checked) {
+    billValue = parseInt(billValue) + parseInt(billValue) * 0.1;
+    console.log(billValue);
+  }
+  if (document.getElementById("fifteen").checked) {
+    billValue = parseInt(billValue) + parseInt(billValue) * 0.15;
+    console.log(billValue);
+  }
+  if (document.getElementById("twentyFive").checked) {
+    billValue = parseInt(billValue) + parseInt(billValue) * 0.25;
+    console.log(billValue);
+  }
+  if (document.getElementById("fifty").checked) {
+    billValue = parseInt(billValue) + parseInt(billValue) * 0.5;
+    console.log(billValue);
   }
 
   //Get number of people
-
   let numberOfPeople = numberOfPeopleEntered.value;
-  console.log(numberOfPeople);
 
   let totPerPerson = billValue / numberOfPeople;
 
@@ -32,10 +48,17 @@ const divideAmongstGroup = () => {
     "$0.00");
 
   let totalPerPerson = (document.getElementById("totalDisplay").innerHTML =
-    "$" + totPerPerson);
+    "$" + totPerPerson.toFixed(2));
 };
 
 //Reset form
 const reset = () => {
-  console.log("reset");
+  document.getElementById("form").reset();
+
+  // let radio = document.getElementsByName("amount");
+
+  // for (let i = 0; radio.length; i++)
+  //   if (radio[i].checked == true) {
+  //     radio[i].checked = false;
+  //   }
 };
